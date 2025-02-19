938fd33f28f6c28f6d0855861a62ced24fca386e
fredy3cifras <superextras2@gmail.com>
2025-02-19 01:21:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const categories = document.querySelectorAll('.category');
-
-    categories.forEach(function (category) {
-        category.addEventListener('click', function () {
-            const content = category.nextElementSibling;
-            content.classList.toggle('show');
+document.addEventListener("DOMContentLoaded", function() {
+    const categories = document.querySelectorAll('.category h2');
+    
+    categories.forEach(category => {
+        category.addEventListener('click', function() {
+            const categoryContent = category.nextElementSibling;
+            categoryContent.classList.toggle('show');
         });
     });
 });
